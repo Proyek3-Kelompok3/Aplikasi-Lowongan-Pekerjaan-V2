8c01aceccb405de51ff66dd9565e9dd15d1024b5
febritoriqkhul <febritoriqkhul@gmail.com>
2020-11-19 15:20:30 +0700

    Add login and register form

--- index.php
+++ index.php
@@ -1,63 +1,75 @@
 <?php
 //koneksi
 session_start();
-include "koneksi.php";
+include "login_form/db_connect.php";
+if (!isset($_SESSION['is_login'])) {
+  header('location:login_form/login.php');
+}
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>SPK TOPSIS</title>
-    <!--bootstrap-->
-    <link href="tampilan/css/bootstrap.min.css" rel="stylesheet">
-    <link href="styles/slider.css" rel="stylesheet" type="text/css" media="all">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  </head>
-  <body><br>
-<!--navbar navbar-default navbar-custom-->
-    <!--menu-->
-    <nav class="">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Sistem Pendukung Keputusan Metode Topsis</a>
-        </div>
+  <title>SPK TOPSIS</title>
+  <!--bootstrap-->
+  <link href="tampilan/css/bootstrap.min.css" rel="stylesheet">
+  <link href="styles/slider.css" rel="stylesheet" type="text/css" media="all">
 
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <a href="kriteria.php">Kriteria</a>
-            </li>
-            <li>
-              <a href="alternatif.php">Alternatif</a>
-            </li>
-            <li>
-              <a href="nilmat.php">Nilai Matriks</a>
-            </li>
-            <li>
-              <a href="hastop.php">Hasil Topsis</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav><br><br>
+</head>
 
+<body><br>
+  <!--navbar navbar-default navbar-custom-->
+  <!--menu-->
+  <nav class="">
     <div class="container">
-    <br>
-      <center><h1><bold>SELAMAT DATANG DI WEBSITE PENGUMUMAN PEREKRUTAN KARYAWAN PT ESKA INDO JAYA KARANGANYAR</bold></h1></center>
-    </div>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Sistem Pendukung Keputusan Metode Topsis</a>
+      </div>
 
-<!--     <div class="tagline">
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="kriteria.php">Kriteria</a>
+          </li>
+          <li>
+            <a href="alternatif.php">Alternatif</a>
+          </li>
+          <li>
+            <a href="nilmat.php">Nilai Matriks</a>
+          </li>
+          <li>
+            <a href="hastop.php">Hasil Topsis</a>
+          </li>
+          <li>
+            <a href="login_form/logout.php">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav><br><br>
+
+  <div class="container">
+    <br>
+    <center>
+      <h1>
+        <bold>SELAMAT DATANG DI WEBSITE PENGUMUMAN PEREKRUTAN KARYAWAN PT ESKA INDO JAYA KARANGANYAR</bold>
+      </h1>
+    </center>
+  </div>
+
+  <!--     <div class="tagline">
         <p>Sebuah platform yang memberikan ulasan dan referensi tentang tempat wisata hits dan kekinian dari seluruh indonesia.</p><br>
       </div>
       <div class="down">
@@ -65,24 +77,25 @@ include "koneksi.php";
       </div>
     </div> -->
 
-    <div>
-        <center><img src="gambar/gambar1.png"></center>
-    </div> <br><br>
-    <!--footer-->
-    <footer class="text-center">
-      <div class="footer-below">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <em>Sistem Pendukung Keputusan Pemilihan Karyawan Menggunakan Metode Topsis</em>
-            </div>
+  <div>
+    <center><img src="gambar/gambar1.png"></center>
+  </div> <br><br>
+  <!--footer-->
+  <footer class="text-center">
+    <div class="footer-below">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <em>Sistem Pendukung Keputusan Pemilihan Karyawan Menggunakan Metode Topsis</em>
           </div>
         </div>
       </div>
-    </footer>
+    </div>
+  </footer>
 
-    <!--plugin-->
-    <script src="tampilan/js/bootstrap.min.js"></script>
+  <!--plugin-->
+  <script src="tampilan/js/bootstrap.min.js"></script>
 
-  </body>
+</body>
+
 </html>
